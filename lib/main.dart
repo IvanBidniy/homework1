884247f9bccfe861dcd7807a57c4de9a38e602ca
IvanBidniy <pubgfawy89@gmail.com>
2025-02-21 09:59:43 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
-import 'package:flutter/material.dart';
-import 'package:clima/screens/loading_screen.dart';
+void main() {
+  List<double> transactions = [100.0, -50.0, 200.0, -30.0, -20.0, 150.0, -70.0];
 
-void main() => runApp(MyApp());
+  List<double> incomes = transactions.where((t) => t > 0).toList();
+  List<double> expenses = transactions.where((t) => t < 0).toList();
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(),
-      home: LoadingScreen(),
-    );
-  }
+  double totalIncome = incomes.fold(0, (sum, t) => sum + t);
+  double totalExpense = expenses.fold(0, (sum, t) => sum + t);
+
+  print("Надходження: $incomes");
+  print("Витрати: $expenses");
+  print("Загальна сума надходжень: $totalIncome");
+  print("Загальна сума витрат: $totalExpense");
 }
